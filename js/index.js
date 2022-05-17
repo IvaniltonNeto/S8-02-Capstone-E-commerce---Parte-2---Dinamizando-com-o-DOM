b37fd81313dcf0b6a98f5f7b0bd038681ef00833
Ivanilton Neto <netijua@gmail.com>
2022-05-17 14:21:54 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 //Menu
 const header = document.createElement("header")
 document.body.appendChild(header)
@@ -10,7 +9,7 @@ header.appendChild(nav)
 const divLogo = document.createElement("div")
 nav.appendChild(divLogo)
 divLogo.classList.add('divLogo')
-divLogo.innerHTML = '<h3>Draken Store</h3>'
+divLogo.innerHTML ='<a href=""</a>' + '<img src="./img/logo.png" alt="" id="imgLogo">'
 
 const ulCabecalho = document.createElement("ul")
 nav.appendChild(ulCabecalho)
@@ -19,33 +18,30 @@ ulCabecalho.classList.add('cabecalho')
 const li = document.createElement("li")
 ulCabecalho.appendChild(li)
 li.classList.add('cabLi')
-li.innerHTML = '<a href="">Todos</a>'
+li.innerHTML = '<a href="#">Todos</a>'
 
 const li1 = document.createElement("li")
 ulCabecalho.appendChild(li1)
 li1.classList.add('cabLi')
-li1.innerHTML = '<a href="">Acessórios</a>'
+li1.innerHTML = '<a href="#">Acessórios</a>'
 
 const li2 = document.createElement("li")
 ulCabecalho.appendChild(li2)
 li2.classList.add('cabLi')
-li2.innerHTML = '<a href="">Calçados</a>'
+li2.innerHTML = '<a href="#">Calçados</a>'
 
 const li3 = document.createElement("li")
 ulCabecalho.appendChild(li3)
 li3.classList.add('cabLi')
-li3.innerHTML = '<a href="">Camisetas</a>'
+li3.innerHTML = '<a href="#">Camisetas</a>'
+
 //-----------------------------------------------------------------------------------------------------//
 
+//Main
 const main = document.createElement("main")
 document.body.appendChild(main)
 console.log(document.body)
 
-//Vitrine
-/*const ul = document.createElement("ul")
-main.appendChild(ul)
-ul.classList.add('vitrine')
-ul.id = 'ul1'*/
 //--------------------------------------------------------------------------------------------------------//
 
 //Barra de pesquisa e botão
@@ -81,6 +77,21 @@ h3.innerText = "Carrinho de compras"
 const ulCar = document.createElement("ul")
 section.appendChild(ulCar)
 ulCar.classList.add('containerCar')
+ulCar.id = 'containerCa'
+
+const divText = document.createElement("div")
+ulCar.appendChild(divText)
+divText.id = 'divText'
+
+const h2Text = document.createElement("h2")
+divText.appendChild(h2Text)
+h2Text.id = 'h2Text'
+h2Text.innerText = 'Carrinho Vázio'
+
+const h5Text = document.createElement("h5")
+divText.appendChild(h5Text)
+h5Text.id = 'h5Text'
+h5Text.innerText = 'Adicione itens'
 
 const divDet = document.createElement("div")
 section.appendChild(divDet)
@@ -110,146 +121,180 @@ let valor = 0
 //--------------------------------------------------------------------------------------------------------//
 
 //Funções
-function vitrineProd(produto){
-    cardProduto(produto)    
-}
-vitrineProd(data)  
 
+//Função iniciar vitrine, adicionar ao carrinho, remoção do carrinho e botões correspondentes
+ inicializarVitrine = (dataB) =>{     
 
-   
-
-   /* for (let i = 0; i < data.length; i++){
-        let filtro = document.getElementById('barPesq').value.trim()          
-            if(filtro == data[i].nameItem){             
-                return cardProduto(data[i])
-                alert("sasdas")
-        }else if(filtro == data[i].tag){
-               return cardProduto(data[i])
-        }
-        
-    }
-   
-  
-})*/
-
-
-function cardProduto(produto){
-const ul = document.createElement("ul")
-main.appendChild(ul)
-ul.classList.add('vitrine')
-ul.id = 'ul1'
-    for (let i = 0; i < produto.length; i++){
-        
-        const li = document.createElement("li")
-        ul.appendChild(li)
-        li.classList.add('products-content')
-
-        const figure = document.createElement("figure")
-        li.appendChild(figure)
-
-        const img = document.createElement("img")
-        figure.appendChild(img)
-
-        const div = document.createElement("div")
-        li.appendChild(div)
-        div.classList.add('info')
-
-        const p = document.createElement("p")
-        div.appendChild(p)
-        p.classList.add('pProducts')
-
-        const h3 = document.createElement("h3")
-        div.appendChild(h3)
-        h3.classList.add('h3Card')
-
-        const span = document.createElement("span")
-        div.appendChild(span)
-        span.classList.add('spanCard')
-
-        const h3p = document.createElement("h3")
-        div.appendChild(h3p)
-        h3p.classList.add('preco')        
-
-        const button = document.createElement("button")
-        div.appendChild(button)
-        button.classList.add('buttonAdCar')
-        
-        img.src          = produto[i].img
-        p.innerText      = produto[i].tag
-        h3.innerText     = produto[i].nameItem
-        span.innerText   = produto[i].description
-        h3p.innerHTML    = `R$ ${produto[i].value}`
-        button.innerText = produto[i].addCart
-
-        //
-        
-        //Carrinho de compras
-            button.addEventListener('click', function(){
-            const liCar = document.createElement("li")
-            ulCar.appendChild(liCar)
-            liCar.classList.add('liProdCar')
-
-            const figure = document.createElement("figure")
-            liCar.appendChild(figure)
-            figure.classList.add('figProdCar')
-
-            const imgCar = document.createElement("img")
-            figure.appendChild(imgCar)
-            imgCar.classList.add('imgProdCar')
-
-            const divCar = document.createElement("div")
-            liCar.appendChild(divCar)
-            divCar.classList.add('divCar')
-
-            const h4 = document.createElement("h4")
-            divCar.appendChild(h4)
-            h4.classList.add('titleCar')
-
-            const h3List = document.createElement("h3")
-            divCar.appendChild(h3List)
-            h3List.classList.add('Hcar')
-            
-            const buttonCar = document.createElement("button")
-            divCar.appendChild(buttonCar)
-            buttonCar.classList.add('buttonCar')
-            buttonCar.innerText = 'Remover produto'
-
-            imgCar.src          = produto[i].img            
-            h4.innerText        = produto[i].nameItem
-            h3List.innerText    = `R$ ${produto[i].value}`
-            //contador de produtos
-            cont++
-            p2.innerHTML = cont
-            //valorDosProdutos            
-            valor += produto[i].value            
-            //Remover Produtos
-            buttonCar.addEventListener('click', function(){
-                liCar.remove()
-                cont--
-                p2.innerHTML = cont
-                let decreValor = produto[i].value
-                valor = valor - decreValor
-                p4.innerHTML = `R$ ${valor}`
-            })
-            p4.innerHTML = `R$ ${valor}`
-        })
-    }   
-}
-buttonPesq.addEventListener('click', function(){
-    
-    /*const ul = document.createElement("ul")
+    const ul = document.createElement("ul")
     main.appendChild(ul)
-    ul.classList.add('vitrine')*/
+    ul.classList.add('vitrine')
+    ul.id = 'ul1'
+    //Criando Card
+    let containerProdutos = document.getElementById('ul1')
+     dataB.map((produto)=>{
+        containerProdutos.innerHTML += `
+        <li class="card-produto">
+            <figure>
+                <img class="imgProdutos" src="`+produto.img+`">
+            </figure>
 
-        let nomeFiltro = document.getElementById('barPesq').value.trim()
-
-        let filtro = data.filter(function(obj) { 
-            
-        return obj.tag == nomeFiltro;
-         
-        });
-        ul1.remove()
-        cardProduto(filtro) 
+            <div class="info">
+                <p class="pProducts">`+produto.tag+`</p>
+                <h3 class="h3Card">`+produto.nameItem+`</h3>
+                <span class="spanCard">`+produto.description+`</span>
+                <h3 class="preco">R$`+produto.value+`</h3>
+                <button key="`+produto.id+`" class="buttonAdCar">Adcionar ao carrinho</button>
+            </div>
+        </li>
+        `;
+    })     
+    //Atualizando o Carrinho
+    atualizarCarrinho = () =>{
+        divText.remove()    
+        let containerCarrinho = document.getElementById('containerCa')
+        containerCarrinho.innerHTML = ""
+        data.map((produto)=>{
+            if(produto.quantidade > 0){
+                //Carrinho de compras            
+                const liCar = document.createElement("li")
+                ulCar.appendChild(liCar)
+                liCar.classList.add('liProdCar')
+    
+                const figure = document.createElement("figure")
+                liCar.appendChild(figure)
+                figure.classList.add('figProdCar')
+    
+                const imgCar = document.createElement("img")
+                figure.appendChild(imgCar)
+                imgCar.classList.add('imgProdutos')
+                imgCar.id = 'imgProdCar'
+    
+                const divCar = document.createElement("div")
+                liCar.appendChild(divCar)
+                divCar.classList.add('divCar')
+    
+                const h4 = document.createElement("h4")
+                divCar.appendChild(h4)
+                h4.classList.add('titleCar')
+    
+                const h3Qnt = document.createElement("h3")
+                divCar.appendChild(h3Qnt)
+                h3Qnt.classList.add('h3Qnt')
+    
+                const h3List = document.createElement("h3")
+                divCar.appendChild(h3List)
+                h3List.classList.add('Hcar')
+                
+                const buttonCar = document.createElement("button")
+                divCar.appendChild(buttonCar)
+                buttonCar.classList.add('buttonCar')
+                buttonCar.innerText = 'Remover produto'
+                                
+                imgCar.src          = produto.img            
+                h4.innerText        = produto.nameItem
+                h3Qnt.innerText     = `x ${produto.quantidade}` 
+                h3List.innerText    = `R$ ${produto.value}`
+                buttonCar.setAttribute('key', produto.id)                     
+                p2.innerHTML = cont                    
+                
+                //Removendo Produtos
+                 buttonCar.addEventListener('click', function(){
+                    let key = this.getAttribute('key') 
+                    
+                    data[key].quantidade--                
+                    cont--
+                    p2.innerHTML = cont
+                    
+                    let decreValor = produto.value
+                    valor = valor - decreValor
+                    p4.innerHTML = `R$ ${valor}`
+                    
+                    h3Qnt.innerText     = `x ${produto.quantidade}`
+                    if(produto.quantidade < 1){
+                        liCar.remove()
+                    }
+                })
+                p4.innerHTML = `R$ ${valor}`              
+            }                      
+        })
+    }
+    //Captura do button, adicinando um evento, pegando um atributo, incrementando um valor, pegando e somando preço, exibindo a contagem de produtos e chamando o card produto carrinho
+    let links = document.getElementsByClassName('buttonAdCar')    
+    for(let i = 0; i < links.length; i++){
+        links[i].addEventListener("click", function(){
+            let key = this.getAttribute('key')
+            data[key].quantidade++ 
+            valor += data[key].value
+            cont++
+            p2.innerText = cont
+            atualizarCarrinho()
         
-})  
+        })
+    }
+ }
+inicializarVitrine(data)
 
+//Botão pesquisa mais função filtro
+buttonPesq.addEventListener('click', function filtros(){       
+    let nomeFiltro= document.getElementById('barPesq').value.trim()
+    
+    if (nomeFiltro == 'Camisetas'){
+        let filtro = data.filter(function(obj) { 
+            return obj.tag == nomeFiltro
+        
+        })
+        ul1.remove()        
+        inicializarVitrine(filtro) 
+    }
+    else if(nomeFiltro == 'Acessórios'){
+        let filtro = data.filter(function(obj) { 
+            return obj.tag == nomeFiltro
+        
+        })
+        ul1.remove()          
+        inicializarVitrine(filtro) 
+    }
+    else if(nomeFiltro != "Acessórios" || nomeFiltro != "Camisetas"){
+        let filtro = data.filter(function(obj) { 
+            return obj.nameItem == nomeFiltro
+        
+        })
+        ul1.remove()          
+        inicializarVitrine(filtro) 
+    }    
+})
+
+//funções Menu
+li.addEventListener('click', function(){
+    ul1.remove()
+    inicializarVitrine(data)
+})
+
+li1.addEventListener('click', function(){
+    let filtro = data.filter(function(obj) { 
+        return obj.tag == 'Acessórios'
+    
+    })
+    ul1.remove()
+    inicializarVitrine(filtro)
+})
+
+li2.addEventListener('click', function(){
+    let filtro = data.filter(function(obj) { 
+        return obj.tag == 'Calçados'
+    
+    })
+    ul1.remove()
+    inicializarVitrine(filtro)
+    let erro = document.getElementById("ul1")
+    erro.innerHTML = '<h1 id="erro">Não temos produtos dessa secção!<h1>'
+})
+
+li3.addEventListener('click', function(){
+    let filtro = data.filter(function(obj) { 
+        return obj.tag == 'Camisetas'
+    
+    })
+    ul1.remove()
+    inicializarVitrine(filtro)
+})
